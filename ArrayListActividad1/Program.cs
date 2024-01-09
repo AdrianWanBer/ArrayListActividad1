f44fcfcc1c76d653b3e0f6310dfeba34d3ef9910
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-01-09 14:13:48 +0100

    no message

---
+++ ArrayListActividad1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace ArrayListActividad1
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            ArrayList provincias = new ArrayList()
+            {
+                "Almería", "Granada", "Málaga","Jaén","Córdoba", "Sevilla", "Cádiz", "Huelva"
+            };
+
+            provincias.Sort();
+            provincias.Insert(0, numeros[0]);
+            provincias.Insert(2, numeros[1]);
+            provincias.Insert(4, numeros[2]);
+            provincias.Insert(6, numeros[3]);
+            provincias.Insert(8, numeros[4]);
+            provincias.Insert(10, numeros[5]);
+            provincias.Insert(12, numeros[6]);
+            provincias.Insert(14, numeros[7]);
+
+
+
+            //Recorredor del ArrayList
+            for ( int i = 0; i < provincias.Count; i++)
+            {
+                Console.WriteLine(provincias[i]);
+            }
+        }
+    }
+}
